04e5c69fbb04e33110ddbb734f2d4e412820555f
dhopz <david_houpapa@hotmail.com>
2021-10-25 17:54:35 +0100

    testing timestamp function

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,13 +1,14 @@
 class Bank
-    attr_reader :balance
-    
+    attr_reader :balance, :transactions
+      
     def initialize()
         @balance = 0
-        @transactions = Hash.new
+        @transactions = []
     end
 
     def deposit(money)
         @balance += money
+        @transactions.push({timestamp:Time.now.strftime("%d/%m/%Y %H-%M-%S"), type:"Deposit", amount: money})
     end
 
     def withdraw(money)
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,16 +1,39 @@
 require 'Bank'
+require 'active_support/time'
+require 'timecop'
 
 describe Bank do
 
     let(:account) { Bank.new } 
-    let(:timestamp) {'10-01-2023 12-22-08'}
-        
+    let(:time_now) { Time.now.strftime("%d/%m/%Y %h-%m-%s") } 
+    
+    before { Timecop.travel(time_now) }
+    after { Timecop.return }
+
+    it "transactions stored" do
+        account.deposit(10)
+        expect(account.transactions).to include(have_key(:timestamp))
+        expect(account.transactions).to include(have_key(:type))
+        expect(account.transactions).to include(have_key(:amount))        
+    end
+
+    it "transactions stored in array" do
+        account.deposit(6.3)
+        expect(account.transactions).to include(include(:amount=>6.3))        
+    end
+
+    it "transaction timestamp recorded" do
+        account.deposit(7.3)
+        expect(account.transactions[timestamp]).to eq :time_now      
+    end
+            
+    
     it 'creates a new bank account' do
         expect(subject.balance).to eq 0
     end
 
-    it 'no transactions' do
-        expect(subject.transactions).empty?
+    it 'transactions to be empty' do
+        expect(subject.transactions).to be_empty
     end
     
     describe "#deposit" do
@@ -19,10 +42,11 @@ describe Bank do
             expect(account.balance).to eq 10
         end
 
-        it "adds a timestamp to transaction" do
-            account.deposit(10)
-            expect(transactions.find { |transaction| transaction[:timestamp]}).to eq timestamp
-        end
+        # it "checks for a timestamp" do
+        #     account.deposit(10)
+        #     puts (Time.now + 1.minute)
+        #     expect(account.transactions.find { |transaction| transaction[:timestamp]}).to be_within(10.seconds).of Time.now
+        # end
 
     end
 
